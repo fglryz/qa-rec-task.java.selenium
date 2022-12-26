70a5e5bed16aafdd187ac0ece57c2286611f9cca
Fatma Guleryuz <fglryz.111@gmail.com>
2022-12-26 11:49:06 +0000

    I fixed the bug. I create for loop to fix bug and did assertion I also created negative scenario

--- src/test/java/org/example/TaskTest.java
+++ src/test/java/org/example/TaskTest.java
@@ -49,11 +49,43 @@ public class TaskTest {
         search.sendKeys("Trafford");
 
         List<WebElement> searchResult = driver.findElements(By.xpath("//div[@class='locationSearchInput_suggestion__QLl_v']/span"));
-        assertTrue(searchResult.get(0).getText().contains("Manchester"));
+           boolean found=false;
+        for (int i = 0; i <searchResult.size() ; i++) {
+            found=searchResult.get(i).getText().contains("Trafford Park");
+            if(found){
+                break;
+            }
+
+        }
+        assertEquals(true,found);
+       // assertTrue(searchResult.get(0).getText().contains("Manchester"));
     }
 
     // Add negative test scenario
-    //@Test
-    //public void negativeTestSearchResults() {
-    //}
-}
+    @Test
+    public void negativeTestSearchResults() {
+        WebElement cookies = driver.findElement(By.id("onetrust-accept-btn-handler"));
+        cookies.click();
+
+        WebElement jobs = driver.findElement(By.xpath("//a[@href='/jobs']"));
+        jobs.click();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(3000));
+        assertEquals("Jobs – Apply for a care job at Cera", driver.getTitle());
+
+        WebElement search = driver.findElement(By.className("location-search-input"));
+        search.sendKeys("Trafford");
+
+        List<WebElement> searchResult = driver.findElements(By.xpath("//div[@class='locationSearchInput_suggestion__QLl_v']/span"));
+        boolean found=false;
+        for (int i = 0; i <searchResult.size() ; i++) {
+            found=searchResult.get(i).getText().contains("Trafford Garden");
+            if(found){
+                break;
+            }
+        }
+        assertEquals(false,found);
+
+    }
+    }
+
